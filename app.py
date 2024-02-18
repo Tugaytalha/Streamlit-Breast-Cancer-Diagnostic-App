6ce582b42c602b93561f77d312151e5b851a1120
Tugay Talha İçen <76882910+Tugaytalha@users.noreply.github.com>
2024-02-19 00:07:40 +0300

    First cersion finished

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import seaborn as sns
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 import streamlit as st
 import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
 
 #   Görev 1: Data Analizi
@@ -12,6 +18,7 @@ def load_data():
     data = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data", header=None)
     return data
 
+st.write('Veri Seti Yükleniyor...')
 data = load_data()
 data.columns = ['id', 'diagnosis', 'radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean', 'smoothness_mean',
                 'compactness_mean', 'concavity_mean', 'concave points_mean', 'symmetry_mean', 'fractal_dimension_mean',
@@ -31,6 +38,7 @@ st.write(data.columns)
 
 #   Görev 2: Data Ön İşlemleri
 
+st.subheader('Veri Ön İşleme Tabi Tutuluyor...')
 # Gereksiz sütunları temizle
 # Analizde bir anlam ifade etmeği için 'id' sütununu kaldır
 cleaned_data = data.drop(columns=['id'])
@@ -57,8 +65,55 @@ sns.scatterplot(x=malignant_data["radius_mean"], y=malignant_data["texture_mean"
 sns.scatterplot(x=benign_data["radius_mean"], y=benign_data["texture_mean"], label='Benign')
 st.pyplot()
 
-# Veriyi ayır
-X = cleaned_data.drop(columns=[1])
-y = cleaned_data["diagnosis"]
-X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Veriyi ayır ve ölçeklendir
+X = cleaned_data.drop(columns=["diagnosis"]).values
+y = cleaned_data["diagnosis"].values
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+X_train, X_test, Y_train, Y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
+
+#   Görev 3: Model Gerçeklemesi
+
+# Model seçimi
+model_name = st.sidebar.selectbox("Model Seç:", ("KNN", "SVM", "Naive Bayes"))
+
+st.write(f"Seçilen Model: {model_name}, Eğitiliyor...")
+if model_name == "KNN":
+    model = KNeighborsClassifier()
+    params = {'n_neighbors': [5, 7, 9, 11, 15], 'weights': ['uniform', 'distance'], 'p': [2, 3, 4]}
+elif model_name == "SVM":
+    model = SVC()
+    params = {'C': [10, 100, 500], 'gamma': [0.01, 0.001, 0.005], 'kernel': ['rbf', 'poly', 'sigmoid'], 'degree': [1, 2, 3], 'decision_function_shape': ['ovo', 'ovr']}
+elif model_name == "Naive Bayes":
+    model = GaussianNB()
+    params = {}
+
+# En iyi parametreleri bul
+grid_search = GridSearchCV(model, params, cv=5)
+grid_search.fit(X_train, Y_train)
+best_params = grid_search.best_params_
+
+# Optimum parametrelerle modeli eğit
+st.write(f"En iyi parametreler: {best_params}")
+model.set_params(**best_params)
+model.fit(X_train, Y_train)
+
+
+#   Görev 4: Model Analizi
+
+# Model sonuçlarını göster
+predictions = model.predict(X_test)
+accuracy = accuracy_score(Y_test, predictions)
+precision = precision_score(Y_test, predictions)
+recall = recall_score(Y_test, predictions)
+f1 = f1_score(Y_test, predictions)
+conf_matrix = confusion_matrix(Y_test, predictions, labels=[1, 0])
+
+st.subheader('Model Sonuçları:')
+st.write(f"Accuracy: {accuracy}")
+st.write(f"Precision: {precision}")
+st.write(f"Recall: {recall}")
+st.write(f"F1 Score: {f1}")
+st.write('Confusion Matrix:')
+st.write(conf_matrix)
